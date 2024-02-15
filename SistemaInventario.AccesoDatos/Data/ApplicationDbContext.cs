b87169ab51825f328661bf21c64296dda6fbfd51
JesúsEduardoCervantesSánchez <cervantessanchezjesuseduardo@gmail.com>
2024-02-15 08:25:49 -0600

    Modelo Bodega Creado

--- SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+using System.Reflection;
 
 namespace SistemaInventario.AccesoDatos
 {
@@ -7,7 +9,14 @@ namespace SistemaInventario.AccesoDatos
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
+        { 
+        }
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int id { get; set; }
+        [Required(ErrorMessage ="El campo nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "El nombre solo se compone de 60 caracteres como maximo")]
+        public String Nombre { get; set; }
+        [Required(ErrorMessage = "El campo descripción es requerido")]
+        [MaxLength(100, ErrorMessage = "La descrición solo se compone de 100 caracteres como maximo")]
+        public String Descripcion { get; set; }
+        [Required(ErrorMessage = "El estado de la bodega es requerido")]
+        public bool Estado { get; set; }
+    }
+}
